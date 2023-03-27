3e41654a8beae7aeb17ea6e3b902627e0ae9eaa7
EMI-INDO <78555833+EMI-INDO@users.noreply.github.com>
2023-03-28 00:03:51 +0700

    banner
    add banner position, size

--- www/emiAdmobPlugin.js
+++ www/emiAdmobPlugin.js
@@ -5,8 +5,8 @@ exports.initialize = function (arg0, success, error) {
     exec(success, error, 'emiAdmobPlugin', 'initialize', [arg0]);
 };
 
-exports.showBannerAd = function (arg0, success, error) {
-    exec(success, error, 'emiAdmobPlugin', 'showBannerAd', [arg0]);
+exports.showBannerAd = function (bannerAdUnitId, size, position, success, error) {
+    exec(success, error, 'emiAdmobPlugin', 'showBannerAd', [bannerAdUnitId, size, position]);
 };
 
 exports.removeBannerAd = function (arg0, success, error) {
