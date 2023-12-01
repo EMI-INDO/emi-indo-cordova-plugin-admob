ffc78d1ed7c301d3b89c3f56e385cff993ff4382
EMI-INDO <78555833+EMI-INDO@users.noreply.github.com>
2023-12-02 03:17:39 +0700

    Update emiAdmobPlugin.js
    banner fix, add Collapsible, add bannerHide()

--- www/emiAdmobPlugin.js
+++ www/emiAdmobPlugin.js
@@ -4,16 +4,8 @@ exports.initialize = function (success, error) {
     exec(success, error, 'emiAdmobPlugin', 'initialize', []);
 };
 
-exports.getIabTfc = function (success, error) {
-    exec(success, error, 'emiAdmobPlugin', 'getIabTfc', []);
-};
-
-exports.showPrivacyOptionsForm = function (success, error) {
-    exec(success, error, 'emiAdmobPlugin', 'showPrivacyOptionsForm', []);
-};
-
-exports.loadAppOpenAd = function (arg0, arg1, arg2, success, error) {
-    exec(success, error, 'emiAdmobPlugin', 'loadAppOpenAd', [arg0, arg1, arg2]);
+exports.loadAppOpenAd = function (arg0, arg1, success, error) {
+    exec(success, error, 'emiAdmobPlugin', 'loadAppOpenAd', [arg0, arg1]);
 };
 
 exports.showAppOpenAd = function (success, error) {
@@ -32,24 +24,28 @@ exports.removeBannerAd = function (arg0, success, error) {
     exec(success, error, 'emiAdmobPlugin', 'removeBannerAd', [arg0]);
 };
 
-exports.loadInterstitialAd = function (arg0, arg1, arg2, success, error) {
-    exec(success, error, 'emiAdmobPlugin', 'loadInterstitialAd', [arg0, arg1, arg2]);
+exports.hideBannerAd = function (success, error) {
+    exec(success, error, 'emiAdmobPlugin', 'hideBannerAd', []);
+};
+
+exports.loadInterstitialAd = function (arg0, arg1, success, error) {
+    exec(success, error, 'emiAdmobPlugin', 'loadInterstitialAd', [arg0, arg1]);
 };
 
 exports.showInterstitialAd = function (success, error) {
     exec(success, error, 'emiAdmobPlugin', 'showInterstitialAd', []);
 };
 
-exports.loadRewardedAd = function (arg0, arg1, arg2, success, error) {
-    exec(success, error, 'emiAdmobPlugin', 'loadRewardedAd', [arg0, arg1, arg2]);
+exports.loadRewardedAd = function (arg0, arg1, success, error) {
+    exec(success, error, 'emiAdmobPlugin', 'loadRewardedAd', [arg0, arg1]);
 };
 
 exports.showRewardedAd = function (success, error) {
     exec(success, error, 'emiAdmobPlugin', 'showRewardedAd', []);
 };
 
-exports.loadRewardedInterstitialAd = function (arg0, arg1, arg2, success, error) {
-    exec(success, error, 'emiAdmobPlugin', 'loadRewardedInterstitialAd', [arg0, arg1, arg2]);
+exports.loadRewardedInterstitialAd = function (arg0, arg1, success, error) {
+    exec(success, error, 'emiAdmobPlugin', 'loadRewardedInterstitialAd', [arg0, arg1]);
 };
 
 exports.showRewardedInterstitialAd = function (success, error) {
@@ -68,6 +64,6 @@ exports.targeting = function (arg0, arg1, arg2, success, error) {
     exec(success, error, 'emiAdmobPlugin', 'targeting', [arg0, arg1, arg2]);
 };
 
-exports.globalSettings = function (arg0, arg1, arg2, success, error) {
-    exec(success, error, 'emiAdmobPlugin', 'globalSettings', [arg0, arg1, arg2]);
+exports.globalSettings = function (arg0, arg1, arg2, arg3, arg4, arg5, success, error) {
+    exec(success, error, 'emiAdmobPlugin', 'globalSettings', [arg0, arg1, arg2, arg3, arg4, arg5]);
 };
