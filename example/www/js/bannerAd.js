b9b0f655c5c426ffc5233cd2a6be9bdc793353b3
EMI-INDO <78555833+EMI-INDO@users.noreply.github.com>
2024-11-10 05:12:13 +0700

    v1.5.5 Stable release, both platforms
    v1.5.5 Stable release, both platforms
    
    New example https://github.com/EMI-INDO/emi-indo-cordova-plugin-admob/tree/main/example/www

---
+++ example/www/js/bannerAd.js
@@ -0,0 +1,155 @@
+// cordova.plugins.emiAdmobPlugin.styleBannerAd({ padding: 50, margins: 50 });  // (Optional only android)
+//cordova.plugins.emiAdmobPlugin.loadBannerAd({config});
+//cordova.plugins.emiAdmobPlugin.showBannerAd(); // default
+//cordova.plugins.emiAdmobPlugin.hideBannerAd(); // default
+//cordova.plugins.emiAdmobPlugin.removeBannerAd(); // default
+
+
+
+function loadBanner() {
+
+    if (typeof cordova !== 'undefined') {
+
+        cordova.plugins.emiAdmobPlugin.loadBannerAd({
+            adUnitId: Banner_ID, //Banner_ID,
+            position: "bottom-center",
+            size: "responsive_adaptive", // autoResize: true (only responsive_adaptive)
+            collapsible: "bottom", // position: top | bottom (disable, empty string)
+            autoResize: true, // default false
+            autoShow: true, // default false
+
+        });
+    }
+
+}
+
+
+
+
+
+/* ///////<<<<  bannerAd position  >>>>>>\\\\\\\
+
+(ANDROID)
+
+top-right
+top-center
+left
+center
+right
+bottom-center
+bottom-right
+
+(IOS)
+
+bottom-center
+top-center
+
+*/
+
+/* ///////<<<<  bannerAd size  >>>>>>\\\\\\\
+
+(ANDROID)
+
+responsive_adaptive
+anchored_adaptive
+full_width_adaptive
+in_line_adaptive
+banner
+large_banner
+medium_rectangle
+full_banner
+leaderboard
+fluid
+
+(IOS)
+
+responsive_adaptive
+in_line_adaptive
+banner
+large_banner
+full_banner
+leaderboard
+
+
+
+*/
+
+
+
+
+/* ///////<<<<  bannerAd event  >>>>>>\\\\\\\
+
+on.banner.load
+on.banner.failed.load
+on.banner.click
+on.banner.close
+on.banner.impression
+on.banner.open
+
+on.banner.revenue
+on.banner.remove
+on.banner.hide
+
+ // New event plugin v1.5.5 or higher
+
+ on.is.collapsible
+ on.bannerAd.responseInfo
+
+ */
+
+
+
+
+// EVENT For example
+
+document.addEventListener('on.banner.load', () => {
+    console.log("on banner load");
+});
+
+
+document.addEventListener('on.is.collapsible', function (event) {
+    // bannerConfig collapsible: "bottom", // position: top | bottom (disable, empty string)
+    console.log("Collapsible Status: " + event.collapsible);
+
+
+});
+
+document.addEventListener('on.banner.failed.load', (error) => {
+    console.log("on.banner.failed.load" + JSON.stringify(error));
+});
+
+
+
+/*
+// DEBUG
+// isResponseInfo: true, // debug Default false
+document.addEventListener('on.bannerAd.responseInfo', (data) => {
+    console.log("on.bannerAd.responseInfo" + JSON.stringify(data));
+    if (window.log) window.log.value += ("\n on.bannerAd.responseInfo" + JSON.stringify(data));
+});
+
+*/
+
+
+
+
+/*
+https://support.google.com/admob/answer/11322405
+
+Turn on the setting for impression-level ad revenue in your AdMob account:
+Sign in to your AdMob account at https://apps.admob.com.
+Click Settings in the sidebar.
+Click the Account tab.
+In the Account controls section, click the Impression-level ad revenue toggle to turn on this setting.
+*/
+
+document.addEventListener('on.banner.revenue', (data) => {
+    
+    console.log(data.value)
+    console.log(data.currencyCode)
+    console.log(data.precision)
+    console.log(data.adUnitId)
+    
+    //console.log("on.banner.revenue" + JSON.stringify(data));
+    if (window.log) window.log.value += ("\n on.banner.revenue" + JSON.stringify(data));
+});
---
+++ example/www/js/interstitialAd.js
@@ -0,0 +1,122 @@
+
+
+
+let isInterstitialLoad = false;
+
+function loadInterstitial() {
+
+    if (typeof cordova !== 'undefined') {
+
+        cordova.plugins.emiAdmobPlugin.loadInterstitialAd({ adUnitId: Interstitial_ID, autoShow: false });
+
+    }
+}
+
+function showInterstitial() {
+
+    if (typeof cordova !== 'undefined') {
+        
+        if (isInterstitialLoad) {
+            cordova.plugins.emiAdmobPlugin.showInterstitialAd();
+        }
+    }
+
+}
+
+/*
+
+on.interstitial.loaded
+on.interstitial.failed.load
+on.interstitial.click
+on.interstitial.dismissed
+on.interstitial.failed.show
+on.interstitial.impression
+on.interstitial.show
+
+// New event plugin v1.5.5 or higher
+
+on.interstitial.revenue
+on.interstitialAd.responseInfo
+
+*/
+
+
+// EVENT For example
+
+document.addEventListener('on.interstitial.loaded', () => {
+    isInterstitialLoad = true;
+    console.log("on interstitial Ad loaded");
+    
+    window.log.value += ("\n on interstitial Ad loaded");
+});
+
+
+// all events that contain the keyword dismissed there is a block to load the ad after it is closed by the user.
+document.addEventListener('on.interstitial.dismissed', () => {
+    isInterstitialLoad = false;
+    console.log("on interstitial Ad dismissed");
+    console.log("you can load ads automatically after the ads are closed by users");
+    
+    loadInterstitial();
+    
+    window.log.value += ("\n you can load ads automatically after the ads are closed by users");
+
+});
+
+
+document.addEventListener('on.interstitial.failed.load', (error) => {
+    isInterstitialLoad = false;
+    console.log("on.interstitial.failed.load" + JSON.stringify(error));
+    
+    window.log.value += ("\n on.interstitial.failed.load" + JSON.stringify(error));
+});
+
+
+document.addEventListener('on.interstitial.failed.show', (error) => {
+    isInterstitialLoad = false;
+    console.log("on.interstitial.failed.show" + JSON.stringify(error));
+    
+    window.log.value += ("\n on.interstitial.failed.show" + JSON.stringify(error));
+});
+
+
+/*
+// DEBUG
+// isResponseInfo: true, // debug Default false
+document.addEventListener('on.interstitialAd.responseInfo', (data) => {
+
+    console.log("on.interstitialAd.responseInfo" + JSON.stringify(data));
+    if (window.log) window.log.value += ("\n on.interstitialAd.responseInfo" + JSON.stringify(data));
+});
+
+*/
+
+
+
+
+
+
+
+/*
+https://support.google.com/admob/answer/11322405
+
+Turn on the setting for impression-level ad revenue in your AdMob account:
+Sign in to your AdMob account at https://apps.admob.com.
+Click Settings in the sidebar.
+Click the Account tab.
+In the Account controls section, click the Impression-level ad revenue toggle to turn on this setting.
+*/
+
+document.addEventListener('on.interstitial.revenue', (data) => {
+
+    console.log(data.value)
+    console.log(data.currencyCode)
+    console.log(data.precision)
+    console.log(data.adUnitId)
+
+   // console.log("on.interstitial.revenue" + JSON.stringify(data));
+    if (window.log) window.log.value += ("\n on.interstitial.revenue" + JSON.stringify(data));
+});
+
+
+
